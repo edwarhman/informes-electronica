eb57ee61e32540e3e43a331da526c98b3394f24f
Emerson Warhman <edwarhman@gmail.com>
2025-02-17 17:22:18 -0400

    agregar analisis de resultados 1 y hojas de datos

--- informe-2/cálculos/respuesta_en_frecuencia.py
+++ informe-2/cálculos/respuesta_en_frecuencia.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import sys
 
-df = pd.read_excel('./Electrónica II - Informe 2 cálculos.xlsx', header=0)
+df = pd.read_excel('./filtro-sallen-key.xlsx', header=0)
 
 
 # Convert the DataFrame to a NumPy array
@@ -11,11 +11,9 @@ data_array = df.to_numpy()
 data_array.sort
 
 # Select rows 13 to 20 (inclusive) and columns from index 3 onwards
-datos_filtro_sallen_key = data_array[12:20, [9,11]]  # Selecting columns 9 and 11 (zero-based indexing)
-datos_filtro_sallen_key_ordenado = datos_filtro_sallen_key[datos_filtro_sallen_key[:, 1].argsort()]
+datos_filtro = data_array  # Selecting columns 9 and 11 (zero-based indexing)
+datos_filtro_ordenado = datos_filtro[datos_filtro[:, 1].argsort()]
 
-datos_filtro_realimentacion_multiple = data_array[20:28, [9,11]]
-datos_filtro_realimentacion_multiple_ordenado = datos_filtro_realimentacion_multiple[datos_filtro_realimentacion_multiple[:, 1].argsort()]
 
 def graficar_respuesta_en_frecuencia(datos, titulo):
     magnitud = datos[:, 0]
@@ -37,5 +35,4 @@ def graficar_respuesta_en_frecuencia(datos, titulo):
     plt.savefig(titulo + '.png', dpi=300, bbox_inches='tight')
     plt.close()
 
-graficar_respuesta_en_frecuencia(datos_filtro_sallen_key_ordenado, 'Respuesta en frecuencia - Filtro Sallen-Key')
-graficar_respuesta_en_frecuencia(datos_filtro_realimentacion_multiple_ordenado, 'Respuesta en frecuencia - Filtro Realimentación Múltiple')
+graficar_respuesta_en_frecuencia(datos_filtro_ordenado, 'Respuesta en frecuencia - Filtro Sallen-Key')
