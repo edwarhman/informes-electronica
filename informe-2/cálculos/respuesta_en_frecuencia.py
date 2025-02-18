cfacd6d9bc6fc169aa3b105ada341d48686a73d4
Emerson Warhman <edwarhman@gmail.com>
2025-02-18 16:51:57 -0400

    completar informe 2

--- informe-2/cálculos/respuesta_en_frecuencia.py
+++ informe-2/cálculos/respuesta_en_frecuencia.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import sys
 
-df = pd.read_excel('./filtro-realimentacion-multiple.xlsx', header=0)
+df = pd.read_excel('./VARIABLES DE ESTADO.xlsx', header=0)
 
 
 # Convert the DataFrame to a NumPy array
@@ -12,12 +12,12 @@ data_array.sort
 
 # Select rows 13 to 20 (inclusive) and columns from index 3 onwards
 datos_filtro = data_array  # Selecting columns 9 and 11 (zero-based indexing)
-datos_filtro_ordenado = datos_filtro[datos_filtro[:, 1].argsort()]
+datos_filtro_ordenado = datos_filtro[datos_filtro[:, 3].argsort()]
 
 
 def graficar_respuesta_en_frecuencia(datos, titulo):
     magnitud = datos[:, 0]
-    frecuencia = datos[:, 1]
+    frecuencia = datos[:, 3]
 
     plt.figure(figsize=(10, 5))
     
@@ -35,4 +35,4 @@ def graficar_respuesta_en_frecuencia(datos, titulo):
     plt.savefig(titulo + '.png', dpi=300, bbox_inches='tight')
     plt.close()
 
-graficar_respuesta_en_frecuencia(datos_filtro_ordenado, 'Respuesta en frecuencia - Filtro Realimentación Múltiple')
+graficar_respuesta_en_frecuencia(datos_filtro_ordenado, 'Respuesta en frecuencia - Filtro de Variables de Estado2 ')
